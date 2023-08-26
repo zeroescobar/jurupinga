7a3c0a16f6b8c06cb934f54f049e1aba3a26a399
zeroescobar <140256721+zeroescobar@users.noreply.github.com>
2023-08-26 08:30:57 -0300

    Delete jurupinga ses directory

--- jurupinga ses/48_distancia.py
+++
@@ -1,7 +0,0 @@
-dist = int(input("Distancia que voce deseja percorrer em Kms: "))
-if dist>= 200:
-    val_km = 0.45
-else:
-    val_km = 0.5
-tot_val = dist*val_km
-print(f"Preço a ser pago: R$ {tot_val:.2f}")
